7c7ae4e84bdf12d2ca09d54f673824bd63f8a2f0
Charlie <charleezyguan@gmail.com>
2021-06-30 13:22:56 -0400

    Ran git initializer with lombok, web, and kafka

---
+++ src/main/java/com/example/wftraining/WfTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.wftraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfTrainingApplication.class, args);
+	}
+
+}
